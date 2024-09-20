f8b1221953bff1344ca608e6abe54a9949e8dd33
Kiefer <kiefer.yap@gmail.com>
2024-09-20 22:21:20 +0900

    #1 WIP on 07-composition-api

--- web-development/front-end/vue-3/07-composition-api/README.md
+++ web-development/front-end/vue-3/07-composition-api/README.md
@@ -1,2 +1,57 @@
 # About
-##
+## Objective
+To learn about the Composition API:
+- Composition API vs Options API
+- `data()`
+- `methods`
+- `computed`
+- `watch`
+- `ref` vs `reactive`
+- Reference properties
+
+## Running the Demo
+- Navigate to the `07-composition-cli` directory in your command line
+- Run `npm install` to install dependencies
+- Run `npm run serve` to run the front end server
+
+# Background
+## Options API
+- The following way of writing Vue's properties is called the **Options API**
+```javascript
+const app = Vue.createApp({
+  data() {
+    return {
+      list: [],
+      newItem: ''
+    }
+  },
+  methods: {
+    addItem() {
+      this.list.push(this.newItem)
+      this.newItem = ''
+    }
+  }
+})
+```
+
+## Problem with Options API
+- Code that should be grouped together are distributed into data(), methods, computed, etc.
+- On larger applications, this unnecessary code splitting can make things messy and difficult to maintain.
+- In addition, reusing logic can be tricky and cumbersome, which makes copy-pasting unavoidable at times.
+
+# Composition API
+## Notable changes
+
+## JavaScript and TypeScript
+
+## data()
+
+## methods
+
+## computed
+
+## watch
+
+## ref() vs reactive()
+
+## Reference Properties
--- web-development/front-end/vue-3/07-composition-api/src/App.vue
+++ web-development/front-end/vue-3/07-composition-api/src/App.vue
@@ -1,18 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <fieldset class="border rounded p-3 mt-3 col-12 col-md-6 offset-md-3">
+      <legend>Vue Application</legend>
+
+      <!-- Input group -->
+      <div class="input-group mb-3">
+        <!-- Input -->
+        <input type="text" class="form-control" placeholder="Enter number from 1-100" v-model.number="newNumber" />
+
+        <!-- Button-->
+        <div class="input-group-append">
+          <button class="btn btn-success" type="button" @click="addNumber">Add number</button>
+        </div>
+      </div>
+
+      <!-- List of added numbers -->
+      <ul class="list-group mb-3">
+        <li class="list-group-item" v-for="item in list">{{ item }}</li>
+      </ul>
+
+      <!-- Computed and watcher -->
+      <ul class="list-group">
+        <!-- Computed property: Average -->
+        <li class="list-group-item">
+          Average: {{ average }}
+        </li>
+
+        <!-- Watcher -->
+        <li class="list-group-item" v-if="newNumber">
+          <span :class="assessmentClass">
+            Assessment: {{ assessment }}
+          </span>
+        </li>
+      </ul>
+    </fieldset>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { Ref, ref, computed, watch } from 'vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+export default {
+  setup() {
+    // data()
+    const newNumber: Ref<string> = ref('')
+    const list: Ref<Array<number>> = ref([])
+
+    // method
+    const addNumber = function () {
+      const number = Number(newNumber.value)
+      if (number < 1 || number > 100) {
+        alert('Please enter a number from 1-100.')
+        return
+      }
+
+      if (!isNaN(number)) {
+        list.value.push(number)
+        newNumber.value = ''
+      } else {
+        alert('Please enter a number.')
+      }
+    }
+
+    // computed
+    const average: Ref<Number> = computed(() => {
+      if (list.value.length === 0) {
+        return 0
+      }
+      return list.value.reduce((a: number, b: number) => a + b, 0) / list.value.length
+    })
+
+    // watcher
+    const assessmentClass: Ref<Array<string>> = ref([])
+    const assessment: Ref<string> = ref('')
+    let timeout = 0
+    watch(newNumber, (newValue: string, oldValue: string) => {
+      clearTimeout(timeout)
+      assessmentClass.value = []
+      assessment.value = '...'
+
+      timeout = setTimeout(() => {
+        const newNumber: number = Number(newValue)
+        const oldNumber: number = Number(oldValue)
+        const highGrade = 95
+        const passingGrade = 70
+        const closeCallGrade = 75
+
+        // Set the assessment and its class
+        assessmentClass.value = ['text-success']
+        assessment.value = 'Passed'
+
+        if (newNumber >= highGrade) {
+          assessmentClass.value = ['text-info', 'fw-bold']
+          assessment.value = 'Passed with flying colors!'
+        }
+        else if (newNumber < passingGrade) {
+          assessmentClass.value = ['text-danger', 'fst-italic']
+          assessment.value = 'Failed...'
+        }
+        else if (newNumber <= closeCallGrade) {
+          assessmentClass.value = ['text-warning']
+          assessment.value = 'Passed (That was close!)'
+        }
+
+        // Log the old and new values
+        console.log(`New value: ${newNumber}, Old value: ${oldNumber}`)
+      }, 500)
+    })
+
+
+    // Exposing functions and variables to the template
+    return {
+      addNumber,
+      newNumber,
+      list,
+      average,
+      assessmentClass,
+      assessment,
+    }
   }
-});
+}
 </script>
 
 <style>
